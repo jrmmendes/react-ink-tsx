074fe2cae2f17f8f0c33e9c80a8191a10055dcec
Mendes <jrmmendes@outlook.com>
2024-11-21 23:04:01 -0300

    feat: update App to use profile from getProfile

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,50 +1,59 @@
-import { Box, Text, useApp, useInput } from 'ink';
-import React, { FC, useState } from 'react';
-import Spinner from 'ink-spinner';
-import ProgressBar from 'ink-progress-bar';
-
-
-const validOptions = ['q', 'n', 'd'];
+import { Box, Text, useApp, useInput } from "ink";
+import React, { FC, useState } from "react";
+import Spinner from "ink-spinner";
+import { getProfile, Actor } from "../data/get-profile";
+import { useEffect } from "react";
+import { useCallback } from "react";
+import { Newline } from "ink";
 
 export const App: FC = () => {
   const { exit } = useApp();
-  const [option, setOption] = useState<string>();
-  const [inputStatus, setInputStatus] = useState<'valid' | 'invalid'>('valid');
 
-  useInput((input, keys) => {
-    if (input === 'q') {
-      return exit();
-    }
+  const [profile, setProfile] = useState<Actor | void>();
+  const [isLoading, setLoading] = useState(true);
 
-    if (keys.backspace || keys.return || keys.tab) {
-      return;
-    }
+  const getProfileCallback = useCallback(
+    async () =>
+      getProfile()
+        .then((response) => {
+          setProfile(response);
+          setLoading(false);
+        })
+        .catch((e) => setLoading(false)),
+    [getProfile],
+  );
 
-    if (!validOptions.includes(input)) setInputStatus('invalid');
-    else setInputStatus('valid');
+  useEffect(() => {
+    getProfileCallback();
+  }, []);
 
-    setOption(input);
-  });
+  useEffect(() => {
+    if (isLoading === false) return exit();
+  }, [isLoading]);
 
   return (
-    <>
-      <Box
-        flexDirection='column'
-        borderStyle='single'
-        padding={1}>
-
-        <Text
-          bold={true} color={inputStatus === 'invalid' ? 'red' : 'green'}
-          underline={true}>
-          Current input is {inputStatus}
-        </Text>
+    <Box flexDirection="column" padding={1}>
+      {isLoading && (
         <Text>
-          <Spinner type='bouncingBall' />
-          <Text bold={true} color='gray'>[Option]</Text> &gt;&gt; {option}
-          <ProgressBar percent={30} />
+          <Spinner type="dots" /> <Text> fetching profile from bsky </Text>
         </Text>
-      </Box>
-      <Text wrap='middle'>Valid inputs: q (quit), n, d</Text>
-    </>
+      )}
+      {profile && (
+        <Text>
+          {[
+            ["Name", profile.displayName],
+            ["Handle", "@" + profile.handle],
+            ["Followers", profile.followersCount],
+            ["Following", profile.followsCount],
+          ].map(([label, value]) => (
+            <Text key={label}>
+              <Text bold>{label}: </Text>
+              <Text color="blue"> {value} </Text>
+              <Newline />
+            </Text>
+          ))}
+        </Text>
+      )}
+    </Box>
   );
-}
+};
